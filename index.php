ecdcacf7384d2a3a419f99e98824a7dc2eefdc15
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-09-19 18:42:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
  <?php 
 include('header.php');
 ?>
-<h1> HW 2 </h1>
+<h1> <body>
+ <?php
+// Defining function
+function whatIsToday(){
+    echo "Today is " . date('l', mktime());
+}
+// Calling function
+whatIsToday();
+?> </h1>
 
 <form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
@@ -19,16 +27,4 @@ Your email address is: <?php echo $_GET["email"]; ?>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script> 
  </body>
-<body>
-</body>
-<body>
- <?php
-// Defining function
-function whatIsToday(){
-    echo "Today is " . date('l', mktime());
-}
-// Calling function
-whatIsToday();
-?>
-</body>
 </html>
