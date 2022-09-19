2d323bf5878646394ccdfce637a4bc257265c3b8
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-09-19 18:41:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ Your email address is: <?php echo $_GET["email"]; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script> 
  </body>
 <body>
+</body>
+<body>
  <?php
 // Defining function
 function whatIsToday(){
