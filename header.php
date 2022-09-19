ec7a04aabbac04e54f845f0554b180421762b8ac
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-09-19 18:24:58 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,6 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
-    <h1>header</h1>
+    <h1><a class="btn btn-primary" href="#" role="button">PHP Post</a></h1>
   </body>
 </html>
