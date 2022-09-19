7fae8771cab39c2257d339613f1b9779e6c93a9c
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-09-19 14:23:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 
-     <?php include ('header.php');?>
+ <?php 
+include('header.php');
+?>
+<h1> test </h1>
     
       <form action"handlepost.php" method="post">
         Name <input type="text" name ="name"><br>
@@ -9,5 +12,5 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script> 
-    </body>
-  </html>
+ </body>
+</html>
