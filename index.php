238d3271c6edf77b6d464c60c32302b54885ac87
ccamlin3 <78323156+ccamlin3@users.noreply.github.com>
2022-09-19 18:13:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 include('header.php');
 ?>
 <h1> Form </h1>
-    
-      <form action"handlepost.php" method="post">
-        Name <input type="text" name ="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-      </form>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script> 
